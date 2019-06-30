6ca75769987539a2e33a26bd98a70068bd392363
alexisbarros <alexisufal@gmail.com>
2019-06-30 12:02:18 -0300

    Remoção de comentários não necessários

--- pre-processing.py
+++ pre-processing.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageEnhance
 from skimage import color, img_as_float
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, Normalizer
 import numpy as np
 import os
 
@@ -18,12 +18,6 @@ for dir in directories:
 
         print(index)
 
-        # Set label to benign (0) or malignant (1)
-        if dir == 'benign/':
-            label = 0
-        else:
-            label = 1
-
         # Get image
         img = Image.open('dataset/' + dir + filename)
 
@@ -31,20 +25,10 @@ for dir in directories:
         img = img.resize((256, 256))
 
         # Change brightness
-        img = ImageEnhance.Brightness(img).enhance(1.5)
+        img = ImageEnhance.Brightness(img).enhance(1.8)
 
         # Convert img in to float and gray
         img = color.rgb2gray(img_as_float(img))
 
-        # Normalize image and reshape
-        normalizer = StandardScaler().fit(img)
-        img_normalized = np.reshape(normalizer.transform(img), 65536)
-
-        # Insert label in img array
-        img_normalized = np.insert(img_normalized, 0, label)
-
-        # Insert img into dataset array
-        dataset.append(img_normalized)
-
-# Save dataset file
-np.savetxt('dataset/skin_cancer_dataset.txt', dataset)
+        img_to_save = Image.fromarray(img / (img.max() / 255.0)).convert('RGB')
+        img_to_save.save('dataset_normalized/' + dir + filename + '.png')
